74c6ec91e605c7bde2ccf2ff90de6bce195565af
Sam Markham <sam.e.markham@gmail.com>
2019-07-11 23:44:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,9 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     self.total += (price * quantity)
-    @items << title
+    quantity.times do
+      @items << title
+    end 
   end
 
   def apply_discount
